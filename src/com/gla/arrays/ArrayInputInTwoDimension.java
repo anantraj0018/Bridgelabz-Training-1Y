51809b8c3999044c7d8ab98ddb2b8f81be6e0276
Anant <anant.rocket264@gmail.com>
2026-02-11 18:33:08 +0530

    [Anant]ClassWork

--- src/com/gla/arrays/ArrayInputInTwoDimension.java
+++ src/com/gla/arrays/ArrayInputInTwoDimension.java
@@ -12,7 +12,7 @@ public class ArrayInputInTwoDimension {
         }
     }
     for(int i=0;i<row;i++){
-        for(int j=0;j<row;j++){
+        for(int j=0;j<col;j++){
             System.out.print(arr[i][j]+" ");
         }
         System.out.println();
